710856353319f7122322a0932e86a68b5496ed10
Emilie Dagenais <emiliedagenais89@gmail.com>
2022-12-23 19:37:47 -0500

    Initial profile page and zodiac data

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import styled from 'styled-components';
+import { format, parseISO } from 'date-fns';
 
 const Profile = () => {
-	return <div>Profile</div>;
-};
+  const { userData, setUserData } = useContext(UserContext);
+  console.log(userData.name);
+  const date = userData.createdAt;
+  const dateFormatted = format(parseISO(date), 'MMM d, yyyy');
 
+  return (
+    <StyledContainer>
+      <h2>Profile</h2>
+      <StyledProfilePic>
+        <img src={userData.picture} />
+      </StyledProfilePic>
+      <StyledDetails>{userData.nickname}</StyledDetails>
+      <StyledDetails>{userData.zodiac.toUpperCase()}</StyledDetails>
+      <StyledDetails>Joined: {dateFormatted}</StyledDetails>
+    </StyledContainer>
+  );
+};
+const StyledContainer = styled.div``;
+const StyledProfilePic = styled.div``;
+const StyledDetails = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+`;
 export default Profile;
