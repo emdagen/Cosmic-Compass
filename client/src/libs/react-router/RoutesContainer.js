03d5d26bb12da5e8e16d5a137447dc2f4d5e95dc
“Jeffrey <jay.zalischi@gmail.com>
2022-12-25 17:40:08 -0500

    set up zodiac page

--- client/src/libs/react-router/RoutesContainer.js
+++ client/src/libs/react-router/RoutesContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import CardDetails from '../../pages/CardDetails.js';
+import CardDetails from '../../pages/CardDetails';
 import Home from '../../pages/Home';
-import Oopsie from '../../pages/Oopsie/index.js';
+import Oopsie from '../../pages/Oopsie';
 import Profile from '../../pages/Profile';
 import Tarot from '../../pages/Tarot';
+import Zodiac from '../../pages/Zodiac';
 import { StyledPage } from '../styled-components/StyledPage';
 
 const RoutesContainer = () => {
@@ -14,6 +15,7 @@ const RoutesContainer = () => {
 				<Route path='/' element={<Home />} />
 				<Route path='/tarot/' element={<Tarot />} />
 				<Route path='/card/:name/:_id' element={<CardDetails />} />
+				<Route path='/zodiac/:sign' element={<Zodiac />} />
 				<Route path='/profile' element={<Profile />} />
 				<Route path='/error' element={<Oopsie />} />
 			</Routes>
