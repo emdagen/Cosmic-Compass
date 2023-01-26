76a28d388c30c8268b5f736f2f1911c4aeeb17d2
Emilie Dagenais <emiliedagenais89@gmail.com>
2023-01-25 21:20:27 -0500

    mui styling zodiac page and added horoscope

--- client/src/pages/Zodiac/index.js
+++ client/src/pages/Zodiac/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import zodiacData from '../../data/zodiacData';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import image from '../../data/images/gemini_dark.jpeg';
 import { useEffect, useState } from 'react';
 import getHandler from '../../utils/http-requests/getHandler';
+//MUI imports
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { ListItem, listItemClasses, ListItemText } from '@mui/material';
 
 const Zodiac = () => {
   const { sign } = useParams();
@@ -22,108 +28,93 @@ const Zodiac = () => {
     };
     getHoroscope();
   }, [date, zodiac]);
-  // console.log(zodiacData);
-  // horoscope && console.log(horoscope.description);
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
   return (
     <div>
       {horoscope && (
-        <StyledContainer>
-          <StyledSign>
-            {letters.map((letter, index) => (
-              <span key={index}>{letter.toUpperCase()}</span>
-            ))}
-          </StyledSign>
-          <StyledDescription>
-            <div>
-              <p>{zodiacObj.description}</p>
-            </div>
-            <div>
-              <StyledHead>TODAY'S HOROSCOPE</StyledHead>
-              <StyledHoroscope>"{horoscope.description}"</StyledHoroscope>
-            </div>
-          </StyledDescription>
-          <StyledGenInfo>
-            {/* TODO Img will be dynamic once entered in db */}
-            <StyledCard src={image} />
-            <div>
-              <p>{zodiacObj.dates}</p>
-              <p>Symbol: {zodiacObj.symbol}</p>
-              <p>Element: {zodiacObj.element}</p>
-              <p>Ruling Planet: {zodiacObj.ruling_planet}</p>
-            </div>
-          </StyledGenInfo>
-          <StyledTrait>
-            <div>
-              <StyledHead>TRAITS</StyledHead>
-              {zodiacObj.traits.map((trait) => {
-                return <p key={trait}>* {trait}</p>;
-              })}
-            </div>
-            <div>
-              <StyledHead>FAMOUS {zodiacObj.zodiac.toUpperCase()}</StyledHead>
-              <p>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid>
+            <StyledItem>
+              {letters.map((letter, index) => (
+                <span key={index}>{letter.toUpperCase()}</span>
+              ))}
+            </StyledItem>
+          </Grid>
+
+          <Grid
+            container
+            spacing={{ xs: 3, md: 8 }}
+            columns={{ xs: 2, sm: 8, md: 12, lg: 16 }}
+            paddingLeft={{ xs: 8, md: 2 }}
+            justifyContent='center'
+            alignItems='stretch'
+          >
+            <Grid item xs={4} sm={6} md={5} lg={5}>
+              <StyledDescription>
+                <p>{zodiacObj.dates}</p>
+                <p>Symbol: {zodiacObj.symbol}</p>
+                <p>Element: {zodiacObj.element}</p>
+                <p>Ruling Planet: {zodiacObj.ruling_planet}</p>
+              </StyledDescription>
+              <StyledDescription>
+                <p>{zodiacObj.description}</p>
+              </StyledDescription>
+              <StyledDescription>
+                <h2>TODAY'S HOROSCOPE</h2>
+                <StyledHoroscope>"{horoscope.description}"</StyledHoroscope>
+              </StyledDescription>
+            </Grid>
+            <Grid item xs={4} sm={4} md={4} lg={4}>
+              <img
+                src={image}
+                style={{ maxWidth: '100%', maxHeight: '100%' }}
+              />
+            </Grid>
+            <Grid item xs={4} sm={8} md={9} lg={5}>
+              <StyledDescription>
+                <h2>TRAITS</h2>
+                {zodiacObj.traits.map((trait) => {
+                  return <p key={trait}>* {trait}</p>;
+                })}
+              </StyledDescription>
+
+              <StyledDescription>
+                <h2>FAMOUS {zodiacObj.zodiac.toUpperCase()}</h2>
                 {zodiacObj.famous.map((fame) => {
                   return <p key={fame}>*{fame}</p>;
                 })}
-              </p>
-            </div>
-          </StyledTrait>
-        </StyledContainer>
+              </StyledDescription>
+            </Grid>
+          </Grid>
+        </Box>
       )}
     </div>
   );
 };
 
-const StyledContainer = styled.div`
+const StyledItem = styled(ListItem)`
   display: flex;
-  flex-grow: 1;
-  gap: 16px;
-  justify-content: space-between;
-  line-height: 1.3em;
-  font-size: 20px;
-`;
-
-const StyledSign = styled.div`
-  display: flex;
+  margin-top: 60px;
   flex-direction: column;
-  /* align-items: center; */
-  width: fit-content;
-  text-align: center;
   font-size: 48px;
   line-height: 1.3em;
-  /* border: 2px solid yellow; */
-  justify-content: center;
-  /* flex-basis: 25%; */
+  width: fit-content;
+  position: absolute;
 `;
-const StyledDescription = styled.div`
-  flex-basis: 25%;
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
-const StyledGenInfo = styled.div`
-  flex-basis: 25%;
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
 
-  align-items: center;
+const StyledDescription = styled(ListItemText)`
+  padding-bottom: 16px;
+  position: relative;
 `;
-const StyledTrait = styled.div`
-  flex-basis: 25%;
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
-const StyledCard = styled.img`
-  /* width: 30%; */
-`;
-const StyledHead = styled.p`
-  margin-bottom: 8px;
-`;
-const StyledHoroscope = styled.p`
+
+const StyledHoroscope = styled(ListItemText)`
   font-style: italic;
-  letter-spacing: 2px;
 `;
-
 export default Zodiac;
