ab7b8bade3ea6f4914edabc5326b912a9c4a121a
“Jeffrey <jay.zalischi@gmail.com>
2023-01-29 12:34:38 -0500

    max-width 📏

--- client/src/libs/styled-components/StyledPage.js
+++ client/src/libs/styled-components/StyledPage.js
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
 
 export const StyledPage = styled.div`
+	max-width: ${({ location }) => {
+		const pathnameArray = location.pathname.split('/');
+		console.log(pathnameArray);
+		const legitCheck = pathnameArray.includes('tarot');
+		if (legitCheck && pathnameArray.length > 2) {
+			return 'none';
+		}
+		return 'var(--width-limit)';
+	}};
 	width: 100%;
 	min-height: var(--container-height);
 	/* max-width: var(--width-limit); */
 	margin: auto;
 	padding: var(--layout-padding);
-	/* border: 1px solid purple; */
+	border: 1px solid purple;
 `;
