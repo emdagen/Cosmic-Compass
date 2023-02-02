b8dcf8e1ff7e0451c1fc5a0a4b4d1fe3cefe231d
“Jeffrey <jay.zalischi@gmail.com>
2023-02-02 14:19:11 -0500

    🧹 cleaned bug 🐛

--- client/src/layout/Navbar/index.js
+++ client/src/layout/Navbar/index.js
@@ -140,7 +140,6 @@ export default function Navbar() {
 							<StyledMenuItems
 								onClick={(e) => {
 									logout({ returnTo: window.location.origin });
-									if (location.pathname !== `/profile`) navigate(`/profile`);
 								}}
 							>
 								<RiDoorOpenFill size={20} />
