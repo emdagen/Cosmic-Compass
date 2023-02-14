57703707a16b5bc171f1992968166aa242a930a8
Emilie Dagenais <emiliedagenais89@gmail.com>
2023-02-13 23:49:56 -0500

    changes to mobile drawer and search bar

--- client/src/components/searchbar/components/Suggestion.js
+++ client/src/components/searchbar/components/Suggestion.js
@@ -2,29 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Suggestion = ({
-	searchValue,
-	suggestion,
-	isSelected,
-	index,
-	...delegated
+  searchValue,
+  suggestion,
+  isSelected,
+  index,
+  ...delegated
 }) => {
-	return (
-		<StyledSuggestion
-			className={isSelected ? 'selected' : undefined}
-			{...delegated}
-		>
-			{suggestion}
-		</StyledSuggestion>
-	);
+  return (
+    <StyledSuggestion
+      className={isSelected ? 'selected' : undefined}
+      {...delegated}
+    >
+      {suggestion}
+    </StyledSuggestion>
+  );
 };
 
 export default Suggestion;
 
 const StyledSuggestion = styled.div`
-	padding: 8px;
-	color: #000;
-	background-color: white;
-	&.selected {
-		background: hsla(50deg, 100%, 80%, 0.25);
-	}
+  padding: 8px;
+  color: #000;
+  backdrop-filter: blur(10px);
+  background-color: rgba(84, 90, 167, 0.7);
+  &.selected {
+    background: #545aa7;
+  }
 `;
--- client/src/layout/Navbar/MobileDrawer.js
+++ client/src/layout/Navbar/MobileDrawer.js
@@ -12,123 +12,135 @@ import SignInOut from '../../libs/auth0/SignInOut';
 
 const list = ['Home', 'About', 'Compatibility', 'Horoscope', 'Tarot'];
 const MobileDrawer = ({ anchor, debounceCloseDrawer }) => {
-	const [toggleDarkMode, theme] = useToggleTheme();
-	const [expanded, setExpanded] = React.useState(false);
+  const [toggleDarkMode, theme] = useToggleTheme();
+  const [expanded, setExpanded] = React.useState(false);
 
-	const handleChange = (panel) => (event, newExpanded) => {
-		setExpanded(newExpanded ? panel : false);
-	};
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
 
-	return (
-		<StyledBox
-			sx={{
-				width: anchor === 'top' || anchor === 'bottom' ? 'auto' : '100%',
-			}}
-			role='presentation'
-			onClick={debounceCloseDrawer}
-		>
-			{list.map((title) => {
-				return (
-					<StyledLink key={title} to={`/${title}`}>
-						{title}
-					</StyledLink>
-				);
-			})}
-			<Accordion
-				onClick={(e) => e.stopPropagation()}
-				expanded={expanded === 'panel1'}
-				onChange={handleChange('panel1')}
-			>
-				<AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
-					<StyledTitle isActive={expanded}>
-						<BiRightArrow />
-						<h1>Zodiac</h1>
-					</StyledTitle>
-				</AccordionSummary>
-				<AccordionDetails>
-					{zodiacIcons.map((zodiac, i) => {
-						return (
-							<StyledZodiacLink
-								key={zodiac.sign}
-								to={`/zodiac/${zodiac.sign.toLowerCase()}`}
-								onClick={debounceCloseDrawer}
-							>
-								{zodiac.icon()}
-								<p>{zodiac.sign}</p>
-							</StyledZodiacLink>
-						);
-					})}
-				</AccordionDetails>
-			</Accordion>
-			<h1 onClick={toggleDarkMode}>{theme ? 'Light' : 'Dark'} mode</h1>
-			<StyledLink to={`/profile`}>Profile</StyledLink>
-			<SignInOut />
-		</StyledBox>
-	);
+  return (
+    <StyledBox
+      sx={{
+        width: anchor === 'top' || anchor === 'bottom' ? 'auto' : '100%',
+        backdropFilter: 'blur(10px)',
+        backgroundColor: 'rgb(84,90,167,0.7)',
+      }}
+      role='presentation'
+      onClick={debounceCloseDrawer}
+    >
+      {list.map((title) => {
+        return (
+          <StyledLink key={title} to={`/${title}`}>
+            {title}
+          </StyledLink>
+        );
+      })}
+      <Accordion
+        onClick={(e) => e.stopPropagation()}
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+        sx={{
+          backdropFilter: 'blur(5px)',
+          backgroundColor: 'rgb(84,90,167,0.4)',
+        }}
+      >
+        <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
+          <StyledTitle isActive={expanded}>
+            <BiRightArrow />
+            <h1>Zodiac</h1>
+          </StyledTitle>
+        </AccordionSummary>
+        <AccordionDetails>
+          {zodiacIcons.map((zodiac, i) => {
+            return (
+              <StyledZodiacLink
+                key={zodiac.sign}
+                to={`/zodiac/${zodiac.sign.toLowerCase()}`}
+                onClick={debounceCloseDrawer}
+              >
+                {zodiac.icon()}
+                <p>{zodiac.sign}</p>
+              </StyledZodiacLink>
+            );
+          })}
+        </AccordionDetails>
+      </Accordion>
+      <h1 onClick={toggleDarkMode}>{theme ? 'Light' : 'Dark'} mode</h1>
+      <StyledLink to={`/profile`}>Profile</StyledLink>
+      <SignInOut />
+    </StyledBox>
+  );
 };
 
 export default MobileDrawer;
 
 const StyledLink = styled(Link)`
-	text-decoration: none;
-	color: #ccc;
-	font-size: 2em;
-	font-weight: bold;
+  text-decoration: none;
+  color: #ccc;
+  font-size: 2em;
+  font-weight: bold;
 `;
 
 const StyledZodiacLink = styled(Link)`
-	text-decoration: none;
-	color: #ccc;
-
-	display: flex;
-	align-items: center;
-	gap: 16px;
-	padding: 4px 4px;
-	p {
-		font-size: 18px;
-	}
-	svg {
-		height: 25px;
-		width: 25px;
-	}
+  text-decoration: none;
+  color: #ccc;
+  /* background-color: #545aa7; */
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 4px 4px;
+  p {
+    font-size: 18px;
+  }
+  svg {
+    height: 25px;
+    width: 25px;
+  }
 `;
 
 const StyledBox = styled(Box)`
-	height: 100%;
-	background-color: none;
-	color: #ccc;
+  height: 100%;
+  background-color: none;
+  color: #ccc;
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	gap: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
 
-	h1,
-	p {
-		cursor: pointer;
-	}
+  h1,
+  p {
+    cursor: pointer;
+  }
 
-	/* .css-sh22l5-MuiButtonBase-root-MuiAccordionSummary-root.Mui-expanded, */
-	.css-sh22l5-MuiButtonBase-root-MuiAccordionSummary-root {
-		min-height: 0;
-	}
+  /* .css-sh22l5-MuiButtonBase-root-MuiAccordionSummary-root.Mui-expanded, */
+  .css-sh22l5-MuiButtonBase-root-MuiAccordionSummary-root {
+    min-height: 0;
+  }
 
-	.css-1elwnq4-MuiPaper-root-MuiAccordion-root {
-		all: unset;
+  .css-1elwnq4-MuiPaper-root-MuiAccordion-root {
+    all: unset;
 
-		.css-o4b71y-MuiAccordionSummary-content.Mui-expanded,
-		.css-o4b71y-MuiAccordionSummary-content {
-			margin: 0;
-		}
-	}
+    .css-o4b71y-MuiAccordionSummary-content.Mui-expanded,
+    .css-o4b71y-MuiAccordionSummary-content {
+      margin: 0;
+    }
+  }
+
+  /* .css-r2pfo9-MuiPaper-root-MuiDrawer-paper {
+    backdrop-filter: blur(10px) !important;
+    background-color: rgba(0, 0, 0, 0.9) !important;
+    background-image: none;
+  } */
 `;
 
 const StyledTitle = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 8px;
-	svg {
-		transform: ${(props) => (props.isActive ? 'rotate(90deg)' : '')};
-	}
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  svg {
+    transform: ${(props) => (props.isActive ? 'rotate(90deg)' : '')};
+  }
 `;
