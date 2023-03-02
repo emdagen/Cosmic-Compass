f784a19682e50d526bf4c44166478496afcd336b
“Jeffrey <jay.zalischi@gmail.com>
2023-03-01 23:02:51 -0500

    center content 🎯

--- client/src/libs/styled-components/StyledPage.js
+++ client/src/libs/styled-components/StyledPage.js
@@ -1,19 +1,22 @@
 import styled from 'styled-components';
 
 export const StyledPage = styled.div`
-  ${({ location }) => {
-    const pathnameArray = location.pathname.split('/');
-    console.log(pathnameArray);
-    const legitCheck = pathnameArray.includes('tarot');
-    if (legitCheck && pathnameArray.length > 3) {
-      return `max-width:none;
+	${({ location }) => {
+		const pathnameArray = location.pathname.split('/');
+		console.log(pathnameArray);
+		const legitCheck = pathnameArray.includes('tarot');
+		if (legitCheck && pathnameArray.length > 3) {
+			return `max-width:none;
       padding: 0px 0px`;
-    }
-    return `max-width:var(--width-limit);
-    padding: var(--layout-padding);`;
-  }};
-  width: 100%;
-  min-height: var(--container-height);
-
-  margin: auto;
+		}
+		return `max-width:var(--width-limit);
+    
+    display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;`;
+	}};
+	width: 100%;
+	min-height: var(--container-height);
+	margin: auto;
 `;
